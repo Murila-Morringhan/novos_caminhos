9afb1c2b1eb76b178c3d179d497b9f68e4c8f6e3
Iury Gomes de Oliveira <murila.adm@gmail.com>
2020-09-20 21:59:52 -0300

    Aprendendo a contribuir nos projetos

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+phpinfo ();
+/* TODO REVISAR CÁLCULO*/
+/* FIXME  CORRIGIR CÁLCULO*/
+/**@author Name <email@email.com> */
+/**
+ * Class constructor.
+ */
+public function __construct(type $ = null)
+{
+    $this-> = $;
+}
+if (condition) {
+    # code...
+}
